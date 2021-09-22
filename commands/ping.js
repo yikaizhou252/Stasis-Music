164c9b6d28de9c825fae273bf86f29a6a8c96f29
Yikai Zhou <48587477+yikaizhou252@users.noreply.github.com>
2021-09-22 14:48:30 -0400

    deleted test files

--- commands/ping.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: 'ping',
-    description: "this is a ping command!",
-    execute(message, args) {
-
-        // code below
-        message.channel.send("you pinged me, little ching chong!");
-    }
-}
